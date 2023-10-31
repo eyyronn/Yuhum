0c04727cb351ba5fafd1daec2ded26d136d53682
Manawkie <samenianjoshua@gmail.com>
2023-10-31 16:26:41 -0700

    Fix some sound volume, added foot sfx

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var click_position = Vector2.AXIS_X
 var speed = Global.SPEED
 @onready var anim = get_node("CollisionShape2D/AnimatedSprite2D")
+@onready var sfx_walk = get_node("Walk")
 
 func _ready():
 	click_position = Vector2(position.x, 0)
@@ -10,7 +11,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("left_click"):
 		click_position = get_global_mouse_position()
-	
+		sfx_walk.play()
 
 	var direction = (click_position - position).normalized()
 	velocity.x = direction.x * speed
@@ -19,9 +20,17 @@ func _process(delta):
 	if position.x != click_position.x:
 		position = position.move_toward(Vector2(click_position.x, Global.FLOOR), delta * speed)
 		anim.play("run")
+		if $Timer.time_left <= 0:
+			$Walk.pitch_scale = randf_range(0.8, 1.2)
+			sfx_walk.play()
+			$Timer.start(0.5)
 		
-	else:
+	if position.x == click_position.x:
 		anim.play("stop")
+		sfx_walk.stop()
+		
+		
+		
 	
 	
 	if round(direction.x) == -1:
