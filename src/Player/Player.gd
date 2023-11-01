9e1d103f4779f7ed6624c49e6803faf2312e128d
Jose <joseearronn@gmail.com>
2023-11-02 00:03:53 +0800

    Add radio

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -16,23 +16,21 @@ func _process(delta):
 	var direction = (click_position - position).normalized()
 	velocity.x = direction.x * speed
 	
-		
 	if position.x != click_position.x:
-		position = position.move_toward(Vector2(click_position.x, Global.FLOOR), delta * speed)
+		position.y = Global.FLOOR + 5
+		position = position.move_toward(Vector2(click_position.x, position.y), delta * speed)
+		
 		anim.play("run")
 		if $Timer.time_left <= 0:
 			$Walk.pitch_scale = randf_range(0.8, 1.2)
 			sfx_walk.play()
 			$Timer.start(0.5)
 		
-	if position.x == click_position.x:
+	else:
+		position.y = Global.FLOOR
 		anim.play("stop")
 		sfx_walk.stop()
 		
-		
-		
-	
-	
 	if round(direction.x) == -1:
 		anim.flip_h = true
 	elif round(direction.x) == 1:
