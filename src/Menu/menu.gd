1927a9e579b82611433494749291d6d64444514a
Rops-11 <jrofer.casio11@gmail.com>
2023-11-03 20:40:39 +0800

    Add Main Menu(Temporary Title Design, Play Button, Few UI Designs)

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+@onready var mouse_in_area = false
 
+func _physics_process(delta):
+	pass
+	
+func _on_area_2d_mouse_entered():
+	mouse_in_area = true
+	get_node('Area2D/CollisionShape2D/AnimationPlayer').play("Pop")
 
+func _on_area_2d_mouse_exited():
+	mouse_in_area = false
+	get_node('Area2D/CollisionShape2D/AnimationPlayer').play("Retract")
+	
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://src/room.tscn")
+		
+		
 
+	
