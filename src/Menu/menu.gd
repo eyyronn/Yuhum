a21cdc79f533c4715f53d06e2d56d3d08e42c603
Jose <joseearronn@gmail.com>
2023-11-11 15:35:52 +0800

    Add credits

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -33,5 +33,6 @@ func _on_area_2d_2_input_event(viewport, event, shape_idx):
 func _ready():
 	get_node("Area2D3/CollisionShape2D/AnimationPlayer").play("FADE")
 		
-
-
+func _on_credits_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://addons/credits/credits-scene/credits.tscn")
